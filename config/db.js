86516066e6a7aa2bf64f26e2c766b8f3a9f3cf28
sulthanularief148 <sulthanularief148@gmail.com>
2025-03-14 08:22:02 +0530

    db updated

--- config/db.js
+++ config/db.js
@@ -8,7 +8,11 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT || 3306,
+    connectTimeout: 10000, // 10 seconds timeout
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 
