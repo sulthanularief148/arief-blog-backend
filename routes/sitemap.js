17b2aad67e37379905a109a2296373654dfd4e0d
Gulam <sulthanularief148@gmail.com>
2025-09-01 13:52:44 +0530

    test

--- routes/sitemap.js
+++ routes/sitemap.js
@@ -1,12 +1,11 @@
-import express from 'express'
-import path from 'path'
+// import express from 'express'
+// import path from 'path'
 
-const router = express.Router();
+// const router = express.Router();
 
-// Serve the generated sitemap
-router.get('/sitemap.xml', (req, res) => {
-  const sitemapPath = path.join(__dirname, '..', 'public', 'sitemap.xml');
-  res.sendFile(sitemapPath);
-});
+// router.get('/sitemap.xml', (req, res) => {
+//   const sitemapPath = path.join(__dirname, '..', 'public', 'sitemap.xml');
+//   res.sendFile(sitemapPath);
+// });
 
-export default router;
+// export default router;
